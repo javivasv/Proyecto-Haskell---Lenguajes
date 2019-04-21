e2ae234412106866a9a1af896596c1d2f50314d8
Javier <javivasv@hotmail.com>
2019-04-20 22:53:53 -0400

    Listas funciones simplify y distrib. Funcion pow casi lista, falta Binomio de Newton

--- Term.hs
+++ Term.hs
@@ -6,32 +6,31 @@
 data Term = Const Int Int | Var String | Fun String Term | Integ Term Term | Sum Term Term | Mult Term Term | Exp Term Int
 
 t :: Term
-	t = Var "t"
+t = Var "t"
 
 u :: Term
-	u = Var "u"
+u = Var "u"
 
 v :: Term
-	v = Var "v"
+v = Var "v"
 
 w :: Term
-	w = Var "w"
+w = Var "w"
 
 x :: Term
-	x = Var "x"
+x = Var "x"
 
 y :: Term
-	y = Var "y"
+y = Var "y"
 
 z :: Term
-	z = Var "z"
+z = Var "z"
 
 sen :: (Term) -> Term
-	sen (t1) = Fun "sen" t1
+sen (t1) = Fun "sen" t1
 
 cosen :: (Term) -> Term
-	cosen (t1) = Fun "cos" t1
-
+cosen (t1) = Fun "cos" t1
 
 instance Show Term where
 	show (Const 0 _) = show 0
@@ -54,18 +53,44 @@ instance Num Term where
 	t1 + t2 = Sum t1 t2
 	t1 * t2 = Mult t1 t2
 	fromInteger i = Const (fromInteger i) 1
-	
 
+-- FUNCION SIMPLIFICACION
 simplify :: (Term) -> Term
-	simplify (Sum (fromInteger 0) t) = simplify t
-	simplify (Sum t (fromInteger 0)) = simplify t
-	simplify (Mult (fromInteger 1) t) = simplify t
-	simplify (Mult t (fromInteger 1)) = simplify t
-	simplify (Exp t (fromInteger 0)) = fromInteger 1
+simplify (Exp t 0) = Const 1 1
+simplify (Exp (Const p q) n) = Const (p ^ n) (q ^ n)
+simplify (Const p q) = Const (div p (gcd p q)) (div q (gcd p q)) 
+simplify (Sum (Const a b) (Const c d)) = Const ((a * (div (lcm b d) b)) + (c * (div (lcm b d) d))) (lcm b d)
+simplify (Sum t (Const 0 1)) = t
+simplify (Sum (Const 0 1) t) = t
+simplify (Mult (Const 1 1) t) = t
+simplify (Mult t (Const 1 1)) = t
 
-	-- FALTA
-
-
+-- FUNCION DISTRIBUTIVA
 distrib :: (Term) -> Term
-	distrib (Mult t1 (Sum t2 t3)) = Sum (Mult t1 t2) (Mult t1 t3)
-	distrib (Mult (Sum t2 t3) t1 = Sum (Mult t2 t1) (Mult t3 t1)
+distrib (Mult t1 (Sum t2 t3)) = Sum (Mult t1 t2) (Mult t1 t3)
+distrib (Mult (Sum t2 t3) t1) = Sum (Mult t2 t1) (Mult t3 t1)
+
+-- FUNCION POTENCIA
+pow :: (Term) -> Term
+pow (Exp (Const t1 t2) n) = Const (t1 ^ n) (t2 ^ n)
+pow (Exp (Exp t n) m) = Exp t (n * m)
+pow (Exp (Mult t1 t2) n) = Mult (Exp t1 n) (Exp t2 n)
+-- pow (Exp (Sum t1 t2) n) = 
+
+-- FUNCION FACTORIAL
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+-- FUNCION SUMATORIA
+summation :: Integer -> Integer -> Integer
+summation x y = summation' x y 0
+
+summation' :: Integer -> Integer -> Integer -> Integer
+summation' x y sum =
+    if (y<x) then
+        sum
+    else
+        summation' x (y-1) (sum+y)
+
+--Mult (Const (factorial n) (Mult (factorial i) (factorial (n - i)))) (Mult (Exp t1 (n - i)) (Exp t2 i)) 
